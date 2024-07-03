3051cffdd4c31a42802652768092ba817e6a42ff
Sofia Bargues <barguessofia@gmail.com>
2024-07-03 04:11:05 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,30 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import circles from "/pattern-circles.svg";
+import bgPattern from "/bg-pattern.svg";
 
 function App() {
   return (
-    <>
-      <div className="bg-[#f1f5fe] h-[470px]  rounded-bl-[300px]  absolute w-full"></div>
-      <div className="min-h-screen w-full relative font-manrope flex justify-center  flex-col">
-        <div className="z-10 absolute m-[28px] h-screen flex justify-around flex-col ">
+    <div className="min-h-screen relative flex flex-col w-full font-manrope">
+      <div className="absolute">
+        <img src={bgPattern} alt="" className="h-[470px]" />
+      </div>
+      <div className="w-full z-10 absolute flex justify-center items-center flex-col">
+        <div className="flex justify-around flex-col ">
           <div className="relative">
-            <img src={circles} alt="" className=" absolute " />
-            <h1 className="">Simple, traffic-based pricing</h1>
+            <img src={circles} alt="" className=" top-0 absolute " />
+            <h1 className="text-2xl text-[#273455] font-bold">
+              Simple, traffic-based pricing
+            </h1>
             <p>Sign-up for our 30-day trial. No credit card required.</p>
           </div>
-          <div>
+          <div className="w-[386px] h-[563px] bg-white border-2 ">
             100K PAGEVIEWS $16.00 / month c Monthly Billing Yearly Billing -25%
             Unlimited websites 100% data ownership Email reports Start my trial
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
